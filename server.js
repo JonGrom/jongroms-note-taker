90cc61af299e4a65c899318e15c0fcc68c460bb3
EnzoAndSo <grommeshjonathan@gmail.com>
2024-08-08 10:58:21 -0500

    wrote routes and tested successfully in postman. TODO: test on webpage and submit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json');
+const { title } = require('process');
 // const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
@@ -16,23 +18,44 @@ app.use(express.static('public'));
 
 //GET landing page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 //GET notes page
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
 //GET notes
 app.get('/api/notes', (req, res) => {
+    console.log(req.body)
+    res.status(200).json(notes);
+    
     //read saved json notes
 })
 
 //POST new note
-app.post('api/notes', (req, res) =>{
+app.post('/api/notes', (req, res) =>{
+    console.log('what', req.body)
+    const { title, text } = req.body
+    if (title && text){
+        const newNote = {
+            title,
+            text,
+        }
+
+        const response = {
+            status: 'success', 
+            body: newNote
+        }
+        notes.push(newNote)
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Could not post new note')
+    }
     //POST new note
 })
+
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
